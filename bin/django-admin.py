c68e876cf43de8ff12314c8085d4b3951c9ac3a6
lgants <lgants@gmail.com>
2017-11-17 20:35:27 -0600

    first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Logan/Development/udemy/try_django_1_11/django-restaurants/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
