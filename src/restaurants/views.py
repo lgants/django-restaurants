afb6aa82c7a3c0cf1a882bb6314484dd35fecbc1
lgants <lgants@gmail.com>
2017-11-18 17:48:53 -0600

    added validators

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -6,17 +6,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView
 
+from .forms import RestaurantCreateForm, RestaurantLocationCreateForm
 from .models import RestaurantLocation
-
-def restaurant_listview(request):
-    template_name = 'restaurants/restaurants_list.html'
-    context = {
-        "object_list": [1, 2, 3]
-    }
-    return render(request, template_name, context)
-
-def home(request):
-    return render(request, "base.html", {})
 
 class RestaurantListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
@@ -25,3 +16,19 @@ class RestaurantListView(LoginRequiredMixin, ListView):
 class RestaurantDetailView(LoginRequiredMixin, DetailView):
      def get_queryset(self):
         return RestaurantLocation.objects.filter(owner=self.request.user)
+
+class RestaurantCreateView(LoginRequiredMixin, CreateView):
+    form_class = RestaurantLocationCreateForm
+    login_url = '/login/'
+    template_name = 'form.html'
+    #success_url = "/restaurants/"
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.owner = self.request.user
+        return super(RestaurantCreateView, self).form_valid(form)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(RestaurantCreateView, self).get_context_data(*args, **kwargs)
+        context['title'] = 'Add Restaurant'
+        return context
