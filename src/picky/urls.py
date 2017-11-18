5070629e80f62f8361c87bbbdac07e280d2d3239
lgants <lgants@gmail.com>
2017-11-18 01:04:17 -0600

    updated routes and models

--- src/picky/urls.py
+++ src/picky/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from restaurants.views import home
+from restaurants.views import restaurant_listview
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/', home),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^restaurants/', restaurant_listview),
 ]
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,5 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import RestaurantLocation
+
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    context = {
+        "object_list": [1, 2, 3]
+    }
+    return render(request, template_name, context)
+
 def home(request):
     return render(request, "base.html", {})
