ff7fe1d11c3bab74f9d0557d736eca9e889968d3
lgants <lgants@gmail.com>
2017-11-18 19:17:39 -0600

    started new app

--- src/restaurants/admin.py
+++ src/restaurants/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Restaurant
+from .models import RestaurantLocation
 
-admin.site.register(Restaurant)
+admin.site.register(RestaurantLocation)
--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -7,17 +7,50 @@ from django.core.urlresolvers import reverse
 from .utils import unique_slug_generator
 from .validators import validate_category
 
-# Create your models here.
-class RestaurantLocation(models.Model):
-    name = models.CharField(max_length=120)
-    location = models.CharField(max_length=120, null=True, blank=True)
-    category = models.CharField(max_length=120, null=True, blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(blank=True, null=True)
+User = settings.AUTH_USER_MODEL
 
-    __str__(self):
+class RestaurantLocationQuerySet(models.query.QuerySet):
+    def search(self, query): #RestaurantLocation.objects.all().search(query) #RestaurantLocation.objects.filter(something).search()
+        if query:
+            query = query.strip()
+            return self.filter(
+                Q(name__icontains=query)|
+                Q(location__icontains=query)|
+                Q(location__iexact=query)|
+                Q(category__icontains=query)|
+                Q(category__iexact=query)|
+                Q(item__name__icontains=query)|
+                Q(item__contents__icontains=query)
+                ).distinct()
+        return self
+
+
+class RestaurantLocationManager(models.Manager):
+    def get_queryset(self):
+        return RestaurantLocationQuerySet(self.model, using=self._db)
+
+    def search(self, query): #RestaurantLocation.objects.search()
+        return self.get_queryset().search(query)
+
+
+class RestaurantLocation(models.Model):
+    owner           = models.ForeignKey(User) # class_instance.model_set.all() # Django Models Unleashed JOINCFE.com
+    name            = models.CharField(max_length=120)
+    location        = models.CharField(max_length=120, null=True, blank=True)
+    category        = models.CharField(max_length=120, null=True, blank=True, validators=[validate_category])
+    timestamp       = models.DateTimeField(auto_now_add=True)
+    updated         = models.DateTimeField(auto_now=True)
+    slug            = models.SlugField(null=True, blank=True)
+    #my_date_field   = models.DateField(auto_now=False, auto_now_add=False)
+
+    objects = RestaurantLocationManager()
+
+    def __str__(self):
         return self.name
+
+    def get_absolute_url(self): #get_absolute_url
+        #return f"/restaurants/{self.slug}"
+        return reverse('restaurants:detail', kwargs={'slug': self.slug})
 
     @property
     def title(self):
@@ -25,9 +58,9 @@ class RestaurantLocation(models.Model):
 
 
 def rl_pre_save_receiver(sender, instance, *args, **kwargs):
-instance.category = instance.category.capitalize()
-if not instance.slug:
-    instance.slug = unique_slug_generator(instance)
+    instance.category = instance.category.capitalize()
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
 
 # def rl_post_save_receiver(sender, instance, created, *args, **kwargs):
 #     print('saved')
